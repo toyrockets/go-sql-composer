c5c8e8c027f10be306fcb019e3c11b87c8370fe6
Ryan Dingman <ryan@toyrockets.com>
2015-10-16 14:17:18 -0700

    Adds test for insert statements with SQL functions.

--- Tests/insert_test.go
+++ Tests/insert_test.go
@@ -67,4 +67,29 @@ func TestInsertWithSubSelect(t *testing.T) {
 
 }
 
+func TestInsertWithFunction(t *testing.T) {
+    statement := sql.Insert("user").Values(map[interface{}]interface{}{
+        "password": sql.Func("crypt", "fooble", sql.Func("gen_salt", "bf")),
+    })
+    SQL, values := statement.GenerateSQL()
+
+    expectedSQL := "insert into \"user\" (password) values (crypt($1, gen_salt($2)))"
+    if SQL != expectedSQL  {
+        t.Error("Expected ", expectedSQL, " got ", SQL)
+    }
+
+	expectedValues := []interface{}{"fooble", "bf"}
+
+	if len(values) != len(expectedValues) {
+        t.Error("Expected ", expectedValues, " got ", values)
+	} else {
+		for index, value := range values {
+			if value != expectedValues[index] {
+		        t.Error("Expected ", expectedValues, " got ", values)
+				break
+			}
+		}
+	}
+
+}
 
