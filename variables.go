fb8d2039fbac152e2cc7dc1932c549c2b8a1a2c6
Ryan Dingman <ryan@toyrockets.com>
2015-10-28 18:20:31 -0700

    Allow pointers to primitive types for SQL variables.

--- variables.go
+++ variables.go
@@ -28,6 +28,39 @@ func SQLVariable(value interface{}) SQLExpression {
 	}
 
 	switch val := value.(type) {
+	case *string:
+		value = *val
+	case *bool:
+		value = *val
+	case *uint:
+		value = *val
+	case *uint8:
+		value = *val
+	case *uint16:
+		value = *val
+	case *uint32:
+		value = *val
+	case *uint64:
+		value = *val
+	case *int:
+		value = *val
+	case *int8:
+		value = *val
+	case *int16:
+		value = *val
+	case *int32:
+		value = *val
+	case *int64:
+		value = *val
+	case *float32:
+		value = *val
+	case *float64:
+		value = *val
+	case *time.Time:
+		value = *val
+	}
+
+	switch val := value.(type) {
 	case string, bool, uint, uint8, uint16, uint32, uint64, int, int8, int16, int32, int64, float32, float64, time.Time:
 		return &SQLValue{value: val}
 	default:
